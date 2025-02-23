c4dd71f40586cfbfbfb7d9267c0fa91892a754f8
Mikalai Yakubau <mikalai.yakubau@ukg.com>
2025-02-23 22:22:05 +0300

    Change routes

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,40 +1,66 @@
-import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
-import { aws_cloudfront as cloudfront, aws_s3 as s3 } from 'aws-cdk-lib';
-import { aws_cloudfront_origins as origins, aws_s3_deployment as s3deploy } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 
 export class MyShopStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, 'MyShopBucket', {
+    const websiteBucket = new s3.Bucket(this, 'MyShopFirstBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      enforceSSL: true,
       websiteIndexDocument: 'index.html',
-      bucketName: 'mikalai-cdk-bucket',
-      publicReadAccess: true,
-      blockPublicAccess: new s3.BlockPublicAccess({
-        blockPublicAcls: false,
-        blockPublicPolicy: false,
-        ignorePublicAcls: false,
-        restrictPublicBuckets: false
-      }),
-      removalPolicy: cdk.RemovalPolicy.DESTROY, 
-      autoDeleteObjects: true
     });
 
-    const distribution = new cloudfront.Distribution(this, 'MyCFDistribution', {
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'OriginAccessIdentity', {
+      comment: 'Allow CloudFront to access the website bucket',
+    });
+
+    // Grant read permissions to CloudFront
+    websiteBucket.grantRead(originAccessIdentity);
+
+    // Create a CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'MyShop-distribution', {
       defaultBehavior: {
-        origin: new origins.S3StaticWebsiteOrigin(bucket),
+        origin: new origins.S3Origin(websiteBucket, {
+          originAccessIdentity,
+        }),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
       },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.minutes(30),
+        },
+      ],
     });
 
-    new cdk.CfnOutput(this, 'CloudFrontURL', {
-      value: distribution.distributionDomainName
+    // Deploy site contents to S3 bucket
+    new s3deploy.BucketDeployment(this, 'MyShop-Bucket-Deployment', {
+      sources: [s3deploy.Source.asset('../dist')],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ['/*'],
     });
 
-    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
-      sources: [s3deploy.Source.asset('../build')],
-      destinationBucket: bucket,
+    // Output the CloudFront URL
+    new cdk.CfnOutput(this, 'DistributionDomainName', {
+      value: distribution.distributionDomainName,
+      description: 'Website URL',
+    });
+
+    // Output the S3 bucket name
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: websiteBucket.bucketName,
+      description: 'Website bucket name',
     });
   }
 }
