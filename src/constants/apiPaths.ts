0431ceaacb8da8466fceccd4ce5bbb52bd37024d
Mikalai Yakubau <mikalai.yakubau@ukg.com>
2025-03-11 23:25:24 +0300

    Feat: add ability to upload csv file

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://6sgrugk136.execute-api.eu-north-1.amazonaws.com/dev",
   order: "https://6sgrugk136.execute-api.eu-north-1.amazonaws.com/dev",
-  import: "https://6sgrugk136.execute-api.eu-north-1.amazonaws.com/dev",
+  import: "https://d3ypw4d1vk.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://6sgrugk136.execute-api.eu-north-1.amazonaws.com/dev",
   cart: "https://6sgrugk136.execute-api.eu-north-1.amazonaws.com/dev",
 };
